3d11b671bde56db75281dc0cdc0758d1a37e15b1
soufyane-felate <soufyanfellat03@gmail.com>
2025-04-23 16:07:33 +0100

    Navigation bar and home page creation started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PostListComponent } from './pages/post-list/post-list.component';
+import { PostDetailComponent } from './pages/post-detail/post-detail.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'posts', component: PostListComponent },
+  { path: 'post/:id', component: PostDetailComponent },
+  { path: 'login', component: LoginComponent },
+
+];
