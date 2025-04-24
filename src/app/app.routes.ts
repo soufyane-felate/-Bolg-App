e1586d447ec21329e9f060d3f732879168faa4b2
hdfaouz <hdfaouzia2@gmail.com>
2025-04-24 12:27:06 +0100

    components crud-post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { PostListComponent } from './pages/post-list/post-list.component';
 import { PostDetailComponent } from './pages/post-detail/post-detail.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CrudPostComponent } from './pages/crud-post/crud-post.component';
 
 
 export const routes: Routes = [
@@ -10,5 +11,7 @@ export const routes: Routes = [
   { path: 'posts', component: PostListComponent },
   { path: 'post/:id', component: PostDetailComponent },
   { path: 'login', component: LoginComponent },
+  {path:"crud-post", component: CrudPostComponent},
+  {path:"home", component: HomeComponent}
 
 ];
---
+++ src/app/pages/crud-post/crud-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-post.component.html',
+  styleUrl: './crud-post.component.css'
+})
+export class CrudPostComponent {
+
+}
