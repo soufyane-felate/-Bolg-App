835862ad2c1049272e2da931edcab669841875e5
hdfaouz <hdfaouzia2@gmail.com>
2025-04-25 16:11:00 +0100

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,16 @@ import { PostDetailComponent } from './pages/post-detail/post-detail.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CrudPostComponent } from './pages/crud-post/crud-post.component';
 
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo:'login', pathMatch:'full' },
   { path: 'posts', component: PostListComponent },
   { path: 'post/:id', component: PostDetailComponent },
   { path: 'login', component: LoginComponent },
-  {path:"crud-post", component: CrudPostComponent},
-  {path:"home", component: HomeComponent}
+  {path:'crud-post', component: CrudPostComponent},
+  {path:'home', component: HomeComponent},
+  {path:'dashboard',component: DashboardComponent}
 
 ];
